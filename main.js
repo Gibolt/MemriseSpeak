0fcbeff0f08153407c1b0662d5c09df123712daf
Gibolt <tjrwh2@gmail.com>
2015-01-02 01:11:53 -0600

    Release 1.0

--- main.js
+++ main.js
@@ -6,14 +6,22 @@ var variable = {
 	regex: new RegExp("\\s", "g"),
 };
 
+var tts = {
+	speak: function(text, lang) {
+		text = text || variable.text;
+		lang = lang || variable.lang || "zh-CN";
+		console.log("Saying: " + text + " in " + lang);
+		chrome.runtime.sendMessage({type:"tts", text:text, lang:lang});
+	},
+}
+
 var functions = {
 	speakIfFresh: function() {
-		functions.testNewWord();
-		if (variable.go) {
+		if (functions.testNewWord()) {
 			console.log("New/Failed Word");
 			functions.setWords();
 			functions.convertLang();
-			functions.speak();
+			tts.speak();
 		}
 	},
 
@@ -24,6 +32,7 @@ var functions = {
 			variable.go = true;
 			variable.el = textEl;
 		}
+		return variable.go;
 	},
 
 	setWords: function() {
@@ -31,41 +40,37 @@ var functions = {
 		variable.lang = document.getElementsByClassName('row-label')[0].innerText;
 	},
 
-	convertLang: function() {
+	convertLang: function(lang) {
+		lang = lang || variable.lang;
 		var conversion = {
 			English: "en",
 			Chinese: "zh-CN",
 		}
-		variable.lang = conversion[variable.lang];
+		variable.lang = conversion[lang];
+		return variable.lang;
 	},
 
-	speak: function(text, lang) {
-		text = text || variable.text;
-		lang = lang || variable.lang || "zh-CN";
-		console.log("Saying: " + text + " in " + lang);
-		chrome.runtime.sendMessage({type:"tts", text:text, lang:lang});
-	},
-	
 	replace: function(str) {
 		return str.replace(variable.regex, "");
 	},
 };
 
 // Speak entered text on correct
-function a() {
+function typingResponse() {
 	if (el = document.getElementsByClassName("typing-wrapper")[0]) {
 		el.onchange = function() {
 			var textEl = document.querySelector("input");
 			var text   = textEl.value;
 			if (textEl.classList.contains("correct")) {
 				console.log("Typing Correct");
-				functions.speak(text);
+				tts.speak(text);
 			}
 		};
 	}
 }
 
-function d() {
+// Speak correct answer on select, header if definition selected
+function selectionResponse() {
 	var el = document.querySelector(".choice.correct");
 	if (el && !el.set) {
 		el.set = true;
@@ -73,11 +78,11 @@ function d() {
 		if (textEl) {
 			var text = textEl.innerText;
 			console.log("Selection Correct Option");
-			functions.speak(text);
+			tts.speak(text);
 		}
-		else if (el = document.querySelector(".qquestion")) {
+		else if (header = document.querySelector(".qquestion")) {
 			console.log("Selection Correct Header");
-			functions.speak(el.innerText);
+			tts.speak(header.innerText);
 		}
 	}
 }
@@ -93,19 +98,19 @@ function wordBoxResponse() {
 		}
 		if (text) {
 			console.log("Multiple Selection");
-			functions.speak(text);
+			tts.speak(text);
 		}
 	}
 }
 
 var timer = {
-	setInterval: function() {
+	setIntervals: function() {
 		setInterval(functions.speakIfFresh, 500);
-		setInterval(a, 200);
-		setInterval(d, 50);
+		setInterval(typingResponse, 200);
+		setInterval(selectionResponse, 50);
 		setInterval(wordBoxResponse, 50);
 	},
 };
 
 console.log("Begin Program");
-timer.setInterval();
+timer.setIntervals();
